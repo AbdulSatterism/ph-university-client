93090e777b2bfdff44b40b7971ae908fdecbfbb1
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2024-07-06 18:50:16 +0600

    authentication ok

---
+++ src/components/form/PHInput.tsx
@@ -0,0 +1,21 @@
+import { Input } from "antd";
+import { Controller } from "react-hook-form";
+
+type TInputProps = {
+  name: string;
+  type: string;
+  label: string;
+};
+const PHInput = ({ name, type, label }: TInputProps) => {
+  return (
+    <div style={{ marginBottom: "20px" }}>
+      {label ? label : null}
+      <Controller
+        name={name}
+        render={({ field }) => <Input {...field} type={type} id={name} />}
+      />
+    </div>
+  );
+};
+
+export default PHInput;
